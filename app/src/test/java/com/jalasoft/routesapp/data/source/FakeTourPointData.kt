c4beb31bb244a3ea5ce0de9b9e346605261d143d
Israel Aguilar <isra.neri2@gmail.com>
2022-10-14 10:41:27 -0400

    Merge develop into feature/46-android-enable-disable-tour-points-on-map

--- app/src/test/java/com/jalasoft/routesapp/data/source/FakeTourPointData.kt
+++ app/src/test/java/com/jalasoft/routesapp/data/source/FakeTourPointData.kt
@@ -8,13 +8,13 @@ import com.jalasoft.routesapp.data.source.FakeRoutesData.coordinatesToLocation
 
 object FakeTourPointData {
     private val location1 = coordinatesToLocation(-16.52035351419114, -68.12580890707301)
-    private val tourPoint1 = TourPointPath("1", "City 1", "13 street", location1, "", "Museum")
+    private val tourPoint1 = TourPointPath("1", "City 1", "13 street", location1, "", null, "Museum")
     private val location2 = coordinatesToLocation(-16.524285569842718, -68.12298370418992)
-    private val tourPoint2 = TourPointPath("2", "City 2", "14 street", location2, "", "Park")
+    private val tourPoint2 = TourPointPath("2", "City 2", "14 street", location2, "", null, "Park")
     val tourPoints = listOf(tourPoint1, tourPoint2)
     private val locationLocal = Location(-16.52035351419114, -68.12580890707301)
-    private val tourPointEntity = TourPointEntity("1", "City 1", "13 street", locationLocal, "http", "park")
-    private val touPointCategoryEntity = TourPointsCategoryEntity("1", "park", "parque")
+    private val tourPointEntity = TourPointEntity("1", "City 1", "13 street", locationLocal, "http", "park", "1")
+    private val touPointCategoryEntity = TourPointsCategoryEntity("1", "park", "parque", "2")
     val tourPointsEntity = listOf(tourPointEntity)
     val tourPointsCategoryEntity = listOf(touPointCategoryEntity)
 }
